a52e66196a9db4178dedb12c450461e0e7b3c701
FranElfers <fransozzan@gmail.com>
2021-06-30 09:32:59 -0300

    osx retoques

--- src/components/Back.js
+++ src/components/Back.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default function Back({to}) {
